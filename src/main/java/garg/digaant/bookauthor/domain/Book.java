f885208e8048cbad5698493e6ccf6eeb5d834e16
Digaant Garg <digaant.garg@gmail.com>
2021-01-01 22:29:54 +0530

    Update 1.0

--- src/main/java/garg/digaant/bookauthor/domain/Book.java
+++ src/main/java/garg/digaant/bookauthor/domain/Book.java
@@ -1,20 +1,25 @@
 package garg.digaant.bookauthor.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
+
 @Entity
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
 
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
         inverseJoinColumns = @JoinColumn(name = "author_id"))
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>();
 
     public Book() {
     }
@@ -24,11 +29,19 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public long getId() {
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -57,17 +70,27 @@ public class Book {
     }
 
     @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
         Book book = (Book) o;
 
-        return id == book.id;
+        return id != null ? id.equals(book.id) : book.id == null;
     }
 
     @Override
     public int hashCode() {
-        return (int) (id ^ (id >>> 32));
+        return id != null ? id.hashCode() : 0;
     }
 }
---
+++ src/main/java/garg/digaant/bookauthor/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package garg.digaant.bookauthor.repositories;
+
+import garg.digaant.bookauthor.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
